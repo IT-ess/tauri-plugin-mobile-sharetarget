a5a018280df03fcc82e0c7bf5655baa6c38b5d42
Itess <me@aloiseau.com>
2025-11-27 19:20:06 +0100

    ios: somewhat working hello world

--- ios/Sources/ExamplePlugin.swift
+++ ios/Sources/ExamplePlugin.swift
@@ -12,9 +12,41 @@ class ExamplePlugin: Plugin {
     let args = try invoke.parseArgs(PingArgs.self)
     invoke.resolve(["value": args.value ?? ""])
   }
+    
+    @objc public func testHelloWorld(_ invoke: Invoke) throws {
+        let args = try invoke.parseArgs(PingArgs.self)
+        let name = args.value ?? "World"
+        
+        if let result = ExamplePlugin.helloWorld(name: name) {
+          invoke.resolve(["value": result])
+        } else {
+          invoke.reject("Failed to call helloWorld")
+        }
+      }
+    
+    @_silgen_name("hello_world_ffi")
+    private static func helloWorldFFI(_ name: UnsafePointer<CChar>) -> UnsafeMutablePointer<CChar>?
+
+    @_silgen_name("free_hello_result_ffi")
+    private static func freeHelloResult(_ result: UnsafeMutablePointer<CChar>)
+
+    static func helloWorld(name: String) -> String? {
+      // Call Rust FFI
+      let resultPtr = name.withCString({ helloWorldFFI($0) })
+
+      // Convert C string to Swift String
+        let result = String(cString: resultPtr.unsafelyUnwrapped)
+
+      // Free the C string
+        freeHelloResult(resultPtr.unsafelyUnwrapped)
+
+      return result
+    }
 }
+
+
 
 @_cdecl("init_plugin_mobile_sharetarget")
 func initPlugin() -> Plugin {
-  return ExamplePlugin()
+      return ExamplePlugin()
 }

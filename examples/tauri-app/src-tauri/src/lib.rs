8776acad7a7b23c62cc61df5191cdbe9b4d08659
Itess <me@aloiseau.com>
2025-11-28 12:38:24 +0100

    chore: cleanup ios only code

--- examples/tauri-app/src-tauri/src/lib.rs
+++ examples/tauri-app/src-tauri/src/lib.rs
@@ -1,6 +1,3 @@
-use tauri_plugin_deep_link::DeepLinkExt;
-use tauri_plugin_mobile_sharetarget::{push_new_intent, IOS_DEEP_LINK_SCHEME};
-
 // Learn more about Tauri commands at https://v2.tauri.app/develop/calling-rust/#commands
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -12,25 +9,30 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_mobile_sharetarget::init())
-        .setup(|app| {
-            let start_urls = app.deep_link().get_current()?;
-            if let Some(urls) = start_urls {
-                println!("deep link URLs: {:?}", urls);
-                if let Some(url) = urls.first() {
-                    if url.scheme().eq(IOS_DEEP_LINK_SCHEME.wait()) {
-                        push_new_intent(url.to_string());
+        .setup(|_app| {
+            #[cfg(target_os = "ios")]
+            {
+                use tauri_plugin_deep_link::DeepLinkExt;
+                use tauri_plugin_mobile_sharetarget::{push_new_intent, IOS_DEEP_LINK_SCHEME};
+                let start_urls = _app.deep_link().get_current()?;
+                if let Some(urls) = start_urls {
+                    println!("deep link URLs: {:?}", urls);
+                    if let Some(url) = urls.first() {
+                        if url.scheme().eq(IOS_DEEP_LINK_SCHEME.wait()) {
+                            push_new_intent(url.to_string());
+                        }
                     }
                 }
-            }
 
-            app.deep_link().on_open_url(move |event| {
-                println!("got new url");
-                if let Some(url) = event.urls().first() {
-                    if url.scheme().eq(IOS_DEEP_LINK_SCHEME.wait()) {
-                        push_new_intent(url.to_string());
+                _app.deep_link().on_open_url(move |event| {
+                    println!("got new url");
+                    if let Some(url) = event.urls().first() {
+                        if url.scheme().eq(IOS_DEEP_LINK_SCHEME.wait()) {
+                            push_new_intent(url.to_string());
+                        }
                     }
-                }
-            });
+                });
+            }
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![greet])
